ae5abdee773fe24c915c621e218ce689e9e4aa06
Henrique Beckmann <henriquebeckmann2@gmail.com>
2022-01-29 22:36:41 -0300

    Implemented network behaviour

--- src/p2p.rs
+++ src/p2p.rs
@@ -67,3 +67,96 @@ impl AppBehaviour {
         behaviour
     }
 }
+
+// incoming event handler
+impl NetworkBehaviourEventProcess<FloodsubEvent> for AppBehaviour {
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        if let FloodsubEvent::Message(msg) = event {
+            if let Ok(resp) = serde_json::from_slice::<ChainResponse>(&msg.data) {
+                if resp.receiver == PEER_ID.to_string() {
+                    info!("Response from {}:", msg.source);
+                    resp.blocks.iter().for_each(|r| info!("{:?}", r));
+
+                    self.app.blocks = self.app.choose_chain(self.app.blocks.clone(), resp.blocks);
+                }
+            } else if let Ok(resp) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
+                info!("sending local chain to {}", msg.source.to_string());
+                let peer_id = resp.from_peer_id;
+                if PEER_ID.to_string() == peer_id {
+                    if let Err(e) = self.response_sender.send(ChainResponse {
+                        blocks: self.app.blocks.clone(),
+                        receiver: msg.source.to_string(),
+                    }) {
+                        error!("error sending response via channel, {}", e);
+                    }
+                }
+            } else if let Ok(block) = serde_json::from_slice::<Block>(&msg.data) {
+                info!("received new block from {}", msg.source.to_string());
+                self.app.try_add_block(block);
+            }
+        }
+    }
+}
+
+impl NetworkBehaviourEventProcess<MdnsEvent> for AppBehaviour {
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(discovered_list) => {
+                for (peer, _addr) in discovered_list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _addr) in expired_list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn get_list_peers(swarm: &Swarm<AppBehaviour>) -> Vec<String> {
+    info!("Discovered Peers:");
+    let nodes = swarm.behaviour().mdns.discovered_nodes();
+    let mut unique_peers = HashSet::new();
+    for peer in nodes {
+        unique_peers.insert(peer);
+    }
+    unique_peers.iter().map(|p| p.to_string()).collect()
+}
+
+pub fn handle_print_peers(swarm: &Swarm<AppBehaviour>) {
+    let peers = get_list_peers(swarm);
+    peers.iter().for_each(|p| info!("{}", p));
+}
+
+pub fn handle_print_chain(swarm: &Swarm<AppBehaviour>) {
+    info!("Local Blockchain:");
+    let pretty_json =
+        serde_json::to_string_pretty(&swarm.behaviour().app.blocks).expect("can jsonify blocks");
+    info!("{}", pretty_json);
+}
+
+pub fn handle_create_block(cmd: &str, swarm: &mut Swarm<AppBehaviour>) {
+    if let Some(data) = cmd.strip_prefix("create b") {
+        let behaviour = swarm.behaviour_mut();
+        let latest_block = behaviour
+            .app
+            .blocks
+            .last()
+            .expect("there is at least one block");
+        let block = Block::new(
+            latest_block.id + 1,
+            latest_block.hash.clone(),
+            data.to_owned(),
+        );
+        let json = serde_json::to_string(&block).expect("can jsonify request");
+        behaviour.app.blocks.push(block);
+        info!("broadcasting new block");
+        behaviour
+            .floodsub
+            .publish(BLOCK_TOPIC.clone(), json.as_bytes());
+    }
+}
